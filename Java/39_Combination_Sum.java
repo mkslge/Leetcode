0a0fc1584017c44b736c8b7b1efb3b2dd3a10388
Mark Seeliger <117680637+mkslge@users.noreply.github.com>
2024-11-22 08:13:03 -0500

    optimize 39_Combination_Sum.java

--- Java/39_Combination_Sum.java
+++ Java/39_Combination_Sum.java
@@ -3,10 +3,10 @@ class Solution {
     List<Integer> subset = new ArrayList<>();
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         //do a dfs on all candidates
-        dfs(candidates, target, 0);
+        dfs(candidates, target, 0,0);
         return sol;
     }
-    public void dfs(int [] candidates,int target, int sum) {
+    public void dfs(int [] candidates,int target, int sum, int start) {
         int l = candidates.length;
         //if we ave gone over we can stop
         if(sum >= target ) {
@@ -16,21 +16,13 @@ class Solution {
             }
             return;
         }
-        //otherwise we check where we're starting
-        int startIndex = 0;
-        if(subset.size() > 0) {
-            for(int i = 0; i < l;i++) {
-                if(candidates[i] == subset.get(subset.size() - 1)) {
-                    startIndex = i;
-                }
-            }
-        }
+        
         //and for each possibility we recursively call 2
         //one where it contains the elemtn and one where it doesnt
-        for(int i = startIndex; i < l;i++) {
+        for(int i = start; i < l;i++) {
             sum += candidates[i];
             subset.add(candidates[i]);
-            dfs(candidates,target,sum);
+            dfs(candidates,target,sum,i);
             sum -= candidates[i];
             subset.remove(subset.size() - 1);
         }
