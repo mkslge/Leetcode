d45e8166351310ce19c7cad00e0ff2203c9858d8
Mark Seeliger <117680637+mkslge@users.noreply.github.com>
2026-01-08 20:42:37 +0100

    Implement canJump function for Jump Game problem

---
+++ C++/55_Jump_Game.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int jumps_left = 0;
+        for(int i = 0; i < nums.size();i++) {
+            if(jumps_left < 0) {
+                return false;
+            } 
+            /* The key idea here is
+            if we have more jumps then the current index, we just keep using those, but
+            if we can gain more, we should just do that by starting from the current idx
+            */ 
+            jumps_left = max(jumps_left, nums[i]);
+            jumps_left--;
+        }
+        return true;
+    }
+};
